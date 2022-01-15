9d8a62e36ab21ccfba54ca4cda9ee86b72101a65
alimardon28 <doniyorovalimardon369@gmail.com>
2022-01-15 07:15:30 +0500

    added js structura

---
+++ js/main.js
@@ -0,0 +1,30 @@
+let tasodifSon = Math.trunc(Math.random()*20)+1;
+
+let imkoniyatlar = 10;
+
+document.querySelector('.inputButton').addEventListener('click' , function(){
+
+let taxminSon =Number (document.querySelector('.inputGroup').value);
+
+ if(!taxminSon){
+    document.querySelector('.inputNatija').textContent = "Son kiritilmadi !";
+ }else if(taxminSon === tasodifSon){
+     document.querySelector('.inputNatija').textContent = 'Tasodifiy son topildi';
+     document.querySelector('body').style.backgroundColor = '#60b347';
+     document.querySelector('.gameSharti').textContent = tasodifSon;
+ }else if(taxminSon > tasodifSon){
+     if(imkoniyatlar>1){
+        document.querySelector('.inputNatija').textContent = 'Katta son kiritildi';
+        imkoniyatlar = imkoniyatlar-1;
+        document.querySelector('.imkoniyatlarDarajasi').textContent = imkoniyatlar;
+     }else{ document.querySelector('.inputNatija').textContent = 'Yutqazdingiz (: !';}
+}else if(taxminSon < tasodifSon){
+    if(imkoniyatlar>1){
+        document.querySelector('.inputNatija').textContent = 'Kichik son kiritildi';
+        imkoniyatlar = imkoniyatlar-1;
+        document.querySelector('.imkoniyatlarDarajasi').textContent = imkoniyatlar;
+    }else{
+        document.querySelector('.inputNatija').textContent = 'Yutqazdingiz (: !';
+    }
+}
+});
