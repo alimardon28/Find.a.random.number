0840556dbe4a9969b154f44ef5ab0dc704141c91
alimardon28 <doniyorovalimardon369@gmail.com>
2022-01-15 09:11:14 +0500

    added refresh button

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-let tasodifSon = Math.trunc(Math.random()*20)+1;
-
+let tasodifSon = Math.trunc(Math.random()*30)+1;
+let rekordDaraja = 0;
 let imkoniyatlar = 10;
 
 document.querySelector('.inputButton').addEventListener('click' , function(){
@@ -11,8 +11,12 @@ let taxminSon =Number (document.querySelector('.inputGroup').value);
  }else if(taxminSon === tasodifSon){
      document.querySelector('.inputNatija').textContent = 'Tasodifiy son topildi';
      document.querySelector('body').style.backgroundColor = '#60b347';
-     document.querySelector('.inputNatija').style.backgroundColor = '#60b347';
      document.querySelector('.gameSharti').textContent = tasodifSon;
+
+     if(imkoniyatlar>rekordDaraja){
+        rekordDaraja = imkoniyatlar;
+        document.querySelector('.balandDaraja').textContent = rekordDaraja;
+     }
  }else if(taxminSon > tasodifSon){
      if(imkoniyatlar>1){
         document.querySelector('.inputNatija').textContent = 'Katta son kiritildi';
@@ -29,4 +33,17 @@ let taxminSon =Number (document.querySelector('.inputGroup').value);
         document.querySelector('.inputNatija').textContent = 'Yutqazdingiz (: !';
     }
 }
+});
+
+document.querySelector('.resultButton').addEventListener('click' , function(){
+    imkoniyatlar = 10;
+    tasodifSon = Math.trunc(Math.random()*20)+1;
+    document.querySelector('.imkoniyatlarDarajasi').textContent = imkoniyatlar;
+
+    document.querySelector('.inputNatija').textContent = 'Oyinni boshlang...';
+    document.querySelector('.gameSharti').textContent ="1 va 30 o'rtasida son o'ylayabman topa olasizmi ? ? ?";
+    document.querySelector('.inputGroup').value = "";
+    document.querySelector('body').style.backgroundColor = "#e9e3e3";
+
+
 });
